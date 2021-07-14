919715107378eaa1e86c09cd64b6c17e01d71ac6
CarloD39 <carlodurand@hotmail.it>
2021-07-14 09:27:59 +0200

    prova

---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <Card
+      v-for="(element, index) in comics"
+      :key="index"
+      :thumb="element.thumb"
+      :price="element.price"
+      :title="element.series"
+      :type="element.type"
+    />
+  </main>
+</template>
+
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: 'Main',
+  components: {
+    Card,
+  },
+  props: {
+    comics: Array,
+  },
+  created() {
+    console.log(this.comics);
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '../style/colors';
+
+main {
+  background-color: black;
+  height: 80px;
+  color: $text-white;
+  z-index: 1;
+}
+</style>
